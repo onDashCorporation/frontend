0e9f6cafeb432e7e1b3e985fb21fc74f700f2081
GeoGustin <geogustinad@gmail.com>
2024-04-16 21:42:03 -0300

    Style: update

--- src/pages/Movimentacao/style.jsx
+++ src/pages/Movimentacao/style.jsx
@@ -1,6 +1,5 @@
 import styled from "styled-components";
-import * as I from "iconoir-react"
-
+import * as I from "iconoir-react";
 
 export const Body = styled.div`
   display: flex;
@@ -27,10 +26,10 @@ export const Section = styled.div`
   gap: 20px;
 `;
 export const SectionConatiner = styled.div`
-display: flex;
-flex-direction: row;
-justify-content:space-between ;
-margin-top: 40px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  margin-top: 40px;
   width: 90%;
   gap: 20px;
 `;
@@ -55,10 +54,14 @@ export const Op = styled.button`
   border-radius: 5px;
   background-color: #d9d9d9;
   cursor: pointer;
-  background-color: ${(props) => (props.select === 'true' || props.select === 'false') ? '#277DFF' : '#d9d9d9'};
-  color: ${(props) => (props.select === 'true' || props.select === 'false') ? '#ffffff' : '#000000'};
-
-
+  background-color: ${(props) =>
+    props.select === "true" || props.select === "false"
+      ? "#277DFF"
+      : "#d9d9d9"};
+  color: ${(props) =>
+    props.select === "true" || props.select === "false"
+      ? "#ffffff"
+      : "#000000"};
 `;
 
 export const InsertContainer = styled.div`
@@ -81,117 +84,132 @@ export const TableContainer = styled.div`
   width: 90%;
   height: 900px;
   margin-bottom: 5%;
-  justify-content:space-between;
+  justify-content: space-between;
   display: flex;
   flex-direction: column;
   cursor: pointer;
-  -webkit-box-shadow: 1px 5px 17px -7px rgba(173,170,173,0.98);
-  -moz-box-shadow: 1px 5px 17px -7px rgba(173,170,173,0.98);
-  box-shadow: 1px 5px 17px -7px rgba(173,170,173,0.98);
+  -webkit-box-shadow: 1px 5px 17px -7px rgba(173, 170, 173, 0.98);
+  -moz-box-shadow: 1px 5px 17px -7px rgba(173, 170, 173, 0.98);
+  box-shadow: 1px 5px 17px -7px rgba(173, 170, 173, 0.98);
 `;
 
 export const StyledTable = styled.table`
   width: 100%;
   border-collapse: collapse;
-
 `;
 
 export const TableHeader = styled.thead`
-    background-color: #f2f2f2;
+  background-color: #f2f2f2;
 `;
-export const Overflow = styled.div`
-
-`;
+export const Overflow = styled.div``;
 export const ThHeader = styled.th`
   padding: 17px;
   font-weight: bold;
   text-align: center;
-  border-top-left-radius: ${({ isFirst }) => isFirst ? '5px' : '0px'};
-  border-top-right-radius: ${({ isLast }) => isLast ? '5px' : '0'};
-
+  border-top-left-radius: ${({ isFirst }) => (isFirst ? "5px" : "0px")};
+  border-top-right-radius: ${({ isLast }) => (isLast ? "5px" : "0")};
 `;
 
+export const TrHeader = styled.tr``;
 
-export const TrHeader = styled.tr`
-
-`;
-
-
-export const TableBody = styled.tbody`
-
-
-`;
-export const Vazio = styled.div`
-
-
-`;
+export const TableBody = styled.tbody``;
+export const Vazio = styled.div``;
 export const TrBody = styled.tr`
- 
-    &:hover {
-      background-color: #fbfbfb;
-    }
-  `;
+  &:hover {
+    background-color: #fbfbfb;
+  }
+`;
 
-  export const ImageCell = styled(I.NavArrowDown)`
+export const ImageCell = styled(I.NavArrowDown)`
   width: 45px;
   height: 30px;
   color: #0e0e0e;
-  `;
-
-export const TableCell = styled.td`
 `;
+
+export const TableCell = styled.td``;
 
 export const StyledTableCell = styled(TableCell)`
-    border-bottom: 1px solid #ddd;
-    padding: 20px;
-    width: 80px;
-    height: 30px;
-    text-align: center;
-   
+  border-bottom: 1px solid #ddd;
+  padding: 20px;
+  width: 80px;
+  height: 30px;
+  text-align: center;
 `;
 export const Test = styled.div`
-text-align: center;
-
+  text-align: center;
+`;
+export const ItemContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+`;
+export const RadiusButto = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 40px;
+  height: 40px;
+  border-radius: 100px;
+  &:hover {
+    transition: all 0.3s ease;
+    transition: all 0.3s ;
+    background-color: #ff0000;
+  }
 `;
 
 export const ItemButton = styled(I.Xmark)`
-width: 40px;
-color: #7a7a7a;
+  width: 40px;
+  color: #7a7a7a;
+
+  &:hover {
+    transition: all 0.3s ease;
+    transition: all 0.3s ;
+    color: #ffffff;
+  }
 `;
 export const Edit = styled(I.EditPencil)`
-width: 20px;
-color: green;
+  width: 20px;
+  color: green;
 `;
 export const Trash = styled(I.TrashSolid)`
-margin-left: 20px;
-color: #cf4848;
-width: 20px;
+  margin-left: 20px;
+  color: #cf4848;
+  width: 20px;
 `;
 export const Active = styled.div`
-width: 10px;
-height: 10px;
-border-radius: 100px;
-background-color: aqua;
+  width: 10px;
+  height: 10px;
+  border-radius: 100px;
+  background-color: aqua;
 `;
 
 export const PaginationConatiner = styled.div`
-width: 100%;
-display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: right;
-gap: 50px;
-`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: right;
+  gap: 50px;
+`;
 export const ButtonContainer = styled.div`
-width: 185px;
-padding: 4px;
-border-radius: 5px;
-background-color: #38AD68;
-color: #ffff;
- &:active {
+  width: 85px;
+  padding: 4px;
+  border-radius: 5px;
+  background-color: #38ad68;
+  color: #ffff;
+  &:active {
     transform: scale(98%);
     transition: transform 0.2s ease;
     background-color: #2e8d56;
-    
   }
-`
+  &:hover {
+    transition: transform 0.2s ease;
+    background-color: #2e8d56;
+  }
+`;
+export const ButtonOp = styled.div`
+  width: 185px;
+`;
+
