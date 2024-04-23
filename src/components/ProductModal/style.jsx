ce155aed0c7672a0ba57cc84355c87079ef22dde
GeoGustin <geogustinad@gmail.com>
2024-04-23 17:13:08 -0300

    Update ProductModal

--- src/components/ProductModal/style.jsx
+++ src/components/ProductModal/style.jsx
@@ -13,92 +13,79 @@ export const Overlay = styled.div`
     right: 0;
     background-color: rgba(0,0,0,0.6);
 `;
-
 export const Modal = styled.div`
   display: flex;
   flex-direction: column;
   background-color: #EBEBEB;
-  width: 60vw;
+  width: 70vw;
   box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
   border-radius: 5px;
-  
-`;
-export const ContainerM = styled.div`
-  width: 90%;
-  margin: 1.5% 5% 0% 5%;
 `;
 
-export const IconClose = styled(I.Xmark)`
- color: #ff0000;
- width: 2rem;
- height: 2rem;
-`;
-export const Close = styled.button`
-  background-color: rgba(0,0,0,0.0);
-  cursor: pointer;
 
-
-`;
 export const ContainerHeader = styled.div`
-margin-top: 20px;
-display: flex;
-flex-direction: row;
-justify-content: center;
-align-items: center;
-width: 100%;
-
-`;
-export const header = styled.div`
-display: flex;
-flex-direction: row;
-align-items: center;
-justify-content: space-between;
-width: 90%;
-`;
-
-
-export const HeaderM = styled.h2`
+  margin-top: 20px;
   display: flex;
   flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+export const header = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
   justify-content: space-between;
-  /* margin-bottom: 2rem; */
+  width: 90%;
 `;
 export const TitleM = styled.h2`
   font-weight: 700;
   font-size: 20px;
   color: #000000;
 `;
-export const Switch = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: right;
+export const Close = styled.button`
+  background-color: rgba(0,0,0,0.0);
+  cursor: pointer;
 `;
-export const SwitchText = styled.text`
-  font-weight: 400;
-
-  color: #6F6F6F;
-  margin-right: 0.5rem;
+export const IconClose = styled(I.Xmark)`
+ color: #ff0000;
+ width: 2rem;
+ height: 2rem;
 `;
 
+export const ContainerM = styled.div`
+  width: 90%;
+  margin: 1.5% 5% 0% 5%;
+`;
 export const Div = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     align-items: center;
     margin-top: 20px;
-
 `;
+
 export const Form = styled.div`
   display: flex;
   flex-direction: column;
   width: 100%;
   gap: 1rem;
 `;
-export const Text = styled.h3`
-  font-weight: 700;
-  font-size: 18px;
-
-  color: #000000;
+export const HeaderM = styled.h2`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: flex-start;
+`;
+export const HeaderContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    padding-right: 4rem;
+    //justify-content: space-between;
+    //align-items: center;
+    //margin-top: 20px;
+    width: 100%;
 `;
 export const NameInput = styled.input`
   font-weight: 400;
@@ -110,7 +97,61 @@ export const NameInput = styled.input`
   margin-bottom: 1rem;
   border-radius: 5px;
 
-  width: 40%;
+  width: 100%;
+`;
+export const CategoryInput = styled.input`
+  font-weight: 400;
+  font-size: 18px;
+  color: #000000;
+  padding: 0.8rem;
+  background-color: #D9D9D9;
+  outline: none;
+  margin-bottom: 1rem;
+  border-radius: 5px;
+
+  width: 60%;
+`;
+
+
+export const Switch = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: right;
+
+  width: 50%;
+`;
+export const SwitchText = styled.text`
+  font-weight: 400;
+
+  color: #6F6F6F;
+  margin-right: 0.5rem;
+`;
+
+
+export const Div2 = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    //align-items: center;
+    //margin-top: 20px;
+`;
+export const Div2Content = styled.div`
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    //justify-content: space-between;
+    //align-items: center;
+    //margin-top: 20px;
+    //width: 20%;
+
+`;
+
+
+export const Text = styled.h3`
+  font-weight: 700;
+  font-size: 18px;
+  color: #000000;
+  text-transform: capitalize;
 
 `;
 export const Inputs = styled.input`
@@ -122,10 +163,11 @@ export const Inputs = styled.input`
   outline: none;
   margin-bottom: 1rem;
   border-radius: 5px;
+  gap: 2rem;
 
-  width: 20%;
-
+  //width: 20%;
 `;
+
 export const Textarea = styled.textarea`
   font-weight: 400;
   font-size: 18px;
@@ -135,13 +177,13 @@ export const Textarea = styled.textarea`
   background-color: #D9D9D9;
   resize: none;
 `;
-export const ConatinerButton = styled.div`
-display: flex;
-justify-content: right;
-align-items: center;
-width: 100%;
-padding: 40px 0px;
 
+export const ContainerButton = styled.div`
+  display: flex;
+  justify-content: right;
+  align-items: center;
+  width: 100%;
+  padding: 40px 0px;
 `;
 export const AddButton = styled.button`
   background-color: #D9D9D9;
