a10f99538f33f8e07a6e2afd9c53703894878463
Arthur <arthurferreira1417@gmail.com>
2024-05-06 17:08:11 -0300

    Feat: fix a styled

--- src/components/header/style.jsx
+++ src/components/header/style.jsx
@@ -5,7 +5,6 @@ export const HeaderContainer = styled.div`
   display: flex;
   align-items: center;
   justify-content: space-between;
-  padding: 10px 20px;
   /* background-color: #f4f7ff; */
   background-color: #ffff;
   border-bottom: 0.5px solid rgba(179, 178, 178, 0.2);
