e8f7aa5bf1cb206abcb8ec25f54804009ed8867e
Gabi Luz <132010966+GabrielaLuzz@users.noreply.github.com>
2024-06-12 22:30:53 -0300

    Update style.jsx

--- src/components/header/style.jsx
+++ src/components/header/style.jsx
@@ -1,4 +1,3 @@
-
 import styled from "styled-components";
 import * as I from "iconoir-react"
 export const HeaderContainer = styled.div`
@@ -10,6 +9,7 @@ export const HeaderContainer = styled.div`
 `;
 
 export const Logo = styled.div`
+margin-left:-500px;
 padding: 10px;
 font-size: 30px;
 font-weight: bold;
@@ -114,6 +114,7 @@ export const Perfil = styled.li`
 `;
 
 export const Cargo = styled.li`
+  margin-left:10px;
   padding-bottom: 15px;
 `;
 
